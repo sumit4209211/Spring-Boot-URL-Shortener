29681113d5aa2a0775222e6efbe7a226bf019a35
varunu28 <varun.u28@gmail.com>
2019-03-31 12:49:18 -0700

    Added code for URL shortener

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Spring-Boot-URL-Shortener
 A URL Shortener built using Spring Boot &amp; Spring Data Redis
+
+#### For Redis setup please refer [Redis Downloads](https://redis.io/download)
+
+#### Demo
+Sending a POST request for a URL to get ID
+
+![POST Demo](Demo/POST%20Demo.png)
+
+Getting back the URL from the ID
+
+![GET Demo](Demo/GET%20Demo.png)
---
+++ src/main/java/com/varun/urlshortener/UrlShortenerResource.java
@@ -0,0 +1,47 @@
+package com.varun.urlshortener;
+
+import com.google.common.hash.Hashing;
+import org.apache.commons.validator.routines.UrlValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.nio.charset.StandardCharsets;
+
+@RequestMapping("/rest/url")
+@RestController
+public class UrlShortenerResource {
+
+    @Autowired
+    StringRedisTemplate redisTemplate;
+
+    @GetMapping("/{id}")
+    public String getUrl(@PathVariable String id) {
+        String url = redisTemplate.opsForValue().get(id);
+
+        if (url == null) {
+            throw new RuntimeException("There is no shorter url for id: " + id);
+        }
+
+        System.out.println("URL Retrieved: " + url);
+        return url;
+    }
+
+    @PostMapping
+    public String create(@RequestBody String url) {
+        UrlValidator urlValidator = new UrlValidator(
+                new String[]{"http", "https"}
+        );
+
+        if (urlValidator.isValid(url)) {
+            String id = Hashing.murmur3_32().hashString(url, StandardCharsets.UTF_8).toString();
+
+            System.out.println("URL Id generated: " + id);
+            redisTemplate.opsForValue().set(id, url);
+
+            return id;
+        }
+
+        throw new RuntimeException("URL Invalid: " + url);
+    }
+}
